4d003da4143cfceed5aa0c0e2415d54c729aff52
Eonyong <unjoo94@naver.com>
2021-12-20 22:15:17 +0900

    not slove

--- 프로그래머스/Lv2/다음 큰 숫자.py
+++ 프로그래머스/Lv2/다음 큰 숫자.py
@@ -1,18 +1,9 @@
 def solution(n):
-    binary = bin(n)[2:]
-    if binary[-2:] == '10':
-        binary = binary[-2:] + binary[:-2]
-    elif binary[-2:] == '01':
-        binary[-2:] = binary[-2:].replace('01', '10')
-    elif binary[-2:] == '11':
-        if '0' in binary:
-            binary = binary[:binary.rindex('0')] + binary[binary.rindex('0') + 1:] + binary[
-                                                                                     binary.rindex('0'):binary.rindex(
-                                                                                         '0') + 1]
-        else:
-            print(bin(1 << len(binary)))
-            print(1 << len(binary) + n >> 1)
-    return int(binary, 2)
+    binary = list(bin(n)[2:])
+    if binary[-2:] == ['1', '1']:
+        print(n + 2)
+        return n + 2
+    return True
 
 
 print(solution(78))
