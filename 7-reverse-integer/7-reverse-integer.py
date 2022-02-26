58d9198dc865f0967486637fff2deb79b5180778
Eonyong <42180383+Eonyong@users.noreply.github.com>
2022-02-26 14:05:54 +0900

    Time: 86 ms (5.37%), Space: 13.9 MB (60.48%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -1,12 +1,5 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        answer = 0
-        is_plus = -1 if x < 0 else 1
-        x = abs(x)
-        while True:
-            d, m = divmod(x, 10)
-            answer = answer * 10 + m
-            if not d:
-                return answer * is_plus if -2**31 <= answer <= 2**31 - 1 else 0
-            else:
-                x = d
+        is_plus = '-' if x < 0 else ''
+        x = int(is_plus + ''.join(reversed(str(abs(x)))))
+        return x if -2**31 <= x <= 2**31 - 1 else 0
