77e4d449acb826ad5827a5e70743acfc843de1a1
Eonyong <42180383+Eonyong@users.noreply.github.com>
2022-04-17 19:42:28 +0900

    [Silver II] Title: 스타트와 링크, Time: 524 ms, Memory: 116360 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,36 @@
+def combi(i, n, m, a_team, players, visited, boards):
+    global answer
+    if i == m:
+        b_team = []
+        for idx in range(n):
+            if not visited[idx]:
+                b_team.append(players[idx])
+        val = 0
+        for s in range(m - 1):
+            for e in range(s + 1, m):
+                a_1, a_2, b_1, b_2 = a_team[s], a_team[e], b_team[s], b_team[e]
+                a_score, b_score = boards[a_1][a_2] + boards[a_2][a_1], boards[b_1][b_2] + boards[b_2][b_1]
+                val += (a_score - b_score)
+        if answer > abs(val):
+            answer = abs(val)
+    else:
+        for j in range(n):
+            if not visited[j] and a_team[0] < m:
+                if not i:
+                    a_team[i], visited[j] = players[j], True
+                    combi(i + 1, n, m, a_team, players, visited, boards)
+                    a_team[i], visited[j] = 0, False
+                elif i and a_team[i - 1] < players[j]:
+                    a_team[i], visited[j] = players[j], True
+                    combi(i + 1, n, m, a_team, players, visited, boards)
+                    a_team[i], visited[j] = 0, False
+
+global answer
+answer = float('inf')
+N = int(input())
+boards = [list(map(int, input().split())) for _ in range(N)]
+total = sum(map(sum, boards))
+players = list(range(N))
+combi(0, N, N // 2, [0] * (N // 2), players, [False] * N, boards)
+
+print(answer)
