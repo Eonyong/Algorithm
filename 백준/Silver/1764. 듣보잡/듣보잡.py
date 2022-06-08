6b09649007168942164572ed0038427c05c965d7
Eonyong <42180383+Eonyong@users.noreply.github.com>
2022-06-08 14:34:42 +0900

    [Silver IV] Title: 듣보잡, Time: 236 ms, Memory: 121980 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+cards = set()
+answer, num = [], 0
+for _ in range(n):
+    cards.add(input())
+for _ in range(m):
+    value = input()
+    if value in cards:
+        answer.append(value)
+        num += 1
+answer.sort()
+print(num)
+for idx in range(num):
+    print(answer[idx])
