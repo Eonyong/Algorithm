883f8f3b90bbb1a62dea2d11b44d5527a122612a
Eonyong <42180383+Eonyong@users.noreply.github.com>
2022-06-14 20:53:37 +0900

    [Silver III] Title: N과 M (4), Time: 180 ms, Memory: 118016 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,13 @@
+def prune(i, n, arr, ls, m):
+    if len(arr) == m:
+        print(*arr)
+    else:
+        for j in range(i, n):
+            arr.append(ls[j])
+            prune(j, n, arr, ls, m)
+            arr.pop()
+
+
+n, m = map(int, input().split())
+
+prune(0, n, [], range(1, n + 1), m)
