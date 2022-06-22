4b91c597aa3b9099e1bb7bf8889fbac37c69e7de
Eonyong <42180383+Eonyong@users.noreply.github.com>
2022-06-22 15:13:07 +0900

    [Silver I] Title: 정수 삼각형, Time: 160 ms, Memory: 116144 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,13 @@
+answer = 0
+n = int(input())
+boards = [list(map(int, input().split())) for _ in range(n)]
+
+for row in range(1, n):
+    for col in range(row + 1):
+        if not col:
+            boards[row][col] += boards[row - 1][col]
+        elif col == row:
+            boards[row][col] += boards[row - 1][col - 1]
+        else:
+            boards[row][col] += max(boards[row - 1][col], boards[row - 1][col - 1])
+print(max(boards[-1]))
