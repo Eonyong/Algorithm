a1526071234e71dcd1dc1aafbc39599eb2bd67c0
Eonyong <42180383+Eonyong@users.noreply.github.com>
2022-07-08 11:55:09 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,15 @@
+def cntSum(m, number):
+    global cnt
+    if number == m:
+        cnt += 1
+    elif number < m:
+        for i in [1, 2, 3]:
+            cntSum(m, number + i)
+
+
+n = int(input())
+boards = [int(input()) for _ in range(n)]
+for board in boards:
+    cnt = 0
+    cntSum(board, 0)
+    print(cnt)
