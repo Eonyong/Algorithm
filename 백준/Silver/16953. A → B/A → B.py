dee5cb16181849c6412210ac41bc3c3a4d87aae1
Eonyong <42180383+Eonyong@users.noreply.github.com>
2022-08-18 15:24:59 +0900

    [Silver II] Title: A → B, Time: 140 ms, Memory: 116540 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def GoToEnd(s, e, cnt):
+    global answer
+    if s == e:
+        answer = min(answer, cnt)
+    elif s < end:
+        for m in [s * 2, 10 * s + 1]:
+            GoToEnd(m, e, cnt + 1)
+
+
+answer = float('inf')
+start, end = map(int, sys.stdin.readline().split())
+GoToEnd(start, end, 1)
+print(answer) if answer != float('inf') else print(-1)
