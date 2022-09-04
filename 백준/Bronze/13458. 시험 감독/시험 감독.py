f910177be097d911a4f1a1b85e4d170f007e5d61
Eonyong <42180383+Eonyong@users.noreply.github.com>
2022-09-04 16:03:58 +0900

    [Bronze II] Title: 시험 감독, Time: 424 ms, Memory: 222152 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -0,0 +1,16 @@
+import sys
+
+answer = 0
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+B, C = map(int, sys.stdin.readline().split())
+
+for student in A:
+    answer += 1
+    student -= B
+    if student > 0:
+        if student % C:
+            answer += student // C + 1
+        else:
+            answer += student // C
+print(answer)
