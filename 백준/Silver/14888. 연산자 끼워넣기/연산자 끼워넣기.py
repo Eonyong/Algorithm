84ef84ec360fe197beb9d26478f53f9d6960a657
Eonyong <42180383+Eonyong@users.noreply.github.com>
2022-09-14 10:33:21 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 1672 ms, Memory: 142176 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,38 +1,27 @@
-from itertools import permutations
-
-answer = []
-N = int(input())
-numbers = list(input().split())
-operates = list(map(int, input().split()))
-opers = []
-for idx in range(len(operates)):
-    if operates[idx]:
-        if idx == 0:
-            for _ in range(operates[idx]):
-                opers.append('+')
-        elif idx == 1:
-            for _ in range(operates[idx]):
-                opers.append('-')
-        elif idx == 2:
-            for _ in range(operates[idx]):
-                opers.append('*')
-        elif idx == 3:
-            for _ in range(operates[idx]):
-                opers.append('//')
+import sys
 
 
-for oper in set(permutations(opers)):
-    deq = ''
-    for i in range(len(numbers)):
-        if i == 0:
-            deq = numbers[i]
-        else:
-            if oper[i - 1] == '//' and int(deq) < 0:
-                deq = str(eval('-1*' + deq))
-                deq = str(-1 * eval(deq + oper[i - 1] + numbers[i]))
-            else:
-                deq = str(eval(deq + oper[i - 1] + numbers[i]))
-    answer.append(int(deq))
+def Yeonsanza(i, n, numbers, operates, oders, value):
+    global maxVal, minVal
+    if i == n:
+        maxVal = max(maxVal, int(value))
+        minVal = min(minVal, int(value))
+    else:
+        for j in range(4):
+            if operates[j]:
+                operates[j] -= 1
+                if value[0] == '-' and j == 3:
+                    Yeonsanza(i + 1, n, numbers, operates, oders, f'-{eval(value[1:] + oders[j] + numbers[i])}')
+                else:
+                    Yeonsanza(i + 1, n, numbers, operates, oders, f'{eval(value + oders[j] + numbers[i])}')
+                operates[j] += 1
 
-print(max(answer))
-print(min(answer))
+
+maxVal, minVal = -float('inf'), float('inf')
+N = int(sys.stdin.readline())
+numbers = list(sys.stdin.readline().split())
+operates = list(map(int, sys.stdin.readline().split()))
+oders = ['+', '-', '*', '//']
+Yeonsanza(1, N, numbers, operates, oders, f'{numbers[0]}')
+print(maxVal)
+print(minVal)
