25c0bdba7ff4763fe9dc44cdf30fa7b7ca6c169e
Eonyong <42180383+Eonyong@users.noreply.github.com>
2022-10-13 16:08:22 +0900

    [Platinum V] Title: 어항 정리, Time: 164 ms, Memory: 116520 KB -BaekjoonHub

--- 백준/Platinum/23291. 어항 정리/어항 정리.py
+++ 백준/Platinum/23291. 어항 정리/어항 정리.py
@@ -3,23 +3,48 @@ from collections import deque
 
 input = sys.stdin.readline
 
+
+def FeedFish(fishes):
+    visited = [[0] * len(fish) for fish in fishes]
+    for row in range(len(fishes)):
+        for col in range(len(fishes[row])):
+            for j, i in [[-1, 0], [0, 1], [1, 0], [0, -1]]:
+                nj, ni = row + j, col + i
+                if 0 <= nj < len(fishes) and 0 <= ni < len(fishes[nj]):
+                    abs_val = (fishes[row][col] - fishes[nj][ni]) // 5
+                    if abs_val > 0:
+                        visited[nj][ni] += abs_val
+                        visited[row][col] -= abs_val
+
+    # 동시에 일어난 어항의 변화를 적용시켜줌
+    for row in range(len(fishes)):
+        for col in range(len(fishes[row])):
+            fishes[row][col] += visited[row][col]
+
+    return fishes
+
+
+
 n, k = map(int, input().split())
 ls = list(map(int, input().split()))
 cnt = 0
 
 while True:
     bowls = []
+    # 값 비교해서 k보다 작거나 같으면 아웃
+    # 그게 아니면 가 장 작은 어항에 물고기 1씩 추가
     ls_max, ls_min = max(ls), min(ls)
     if ls_max - ls_min <= k:
         break
-        # pass
     else:
         cnt += 1
         for idx in range(n):
             ls[idx] += 1 if ls[idx] == ls_min else 0
+
+    # 공중부양 수행하는데 가장 아랫단의 어항보다
+    # 공중부양 어항의 길이가 길면 나온다
     while True:
         length = len(bowls[0]) if bowls else 1
-        remains = []
         tmp, remains = ls[:length], ls[length:]
         bowls.append(tmp)
         ls = remains[:]
@@ -30,20 +55,8 @@ while True:
         bowls = list(map(list, zip(*bowls[::-1])))
 
     # 인접한 물고기 주고 받기
-    H = len(bowls)
-    visited = [[0] * len(bowl) for bowl in bowls]
-    for row in range(H):
-        for col in range(len(bowls[row])):
-            for j, i in [[-1, 0], [0, 1], [1, 0], [0, -1]]:
-                nj, ni = row + j, col + i
-                if 0 <= nj < H and 0 <= ni < len(bowls[nj]):
-                    abs_val = (bowls[row][col] - bowls[nj][ni]) // 5
-                    if abs_val > 0:
-                        visited[nj][ni] += abs_val
-                        visited[row][col] -= abs_val
-    for row in range(H):
-        for col in range(len(bowls[row])):
-            bowls[row][col] += visited[row][col]
+    bowls = FeedFish(bowls)
+
 
     # 한 줄로 만드는 행위
     bowls = [deque(bowl) for bowl in bowls]
@@ -66,20 +79,9 @@ while True:
         tmp.appendleft(lft)
         tmp.append(rgt)
     bowls = list(tmp)
-    visited = [[0 for _ in range(len(bowls[0]))] for _ in range(len(bowls))]
-    for row in range(len(bowls)):
-        for col in range(len(bowls[row])):
-            for j, i in [[-1, 0], [0, 1], [1, 0], [0, -1]]:
-                nj, ni = row + j, col + i
-                if 0 <= nj < len(bowls) and 0 <= ni < len(bowls[row]):
-                    abs_val = (bowls[row][col] - bowls[nj][ni]) // 5
-                    if abs_val > 0:
-                        visited[nj][ni] += abs_val
-                        visited[row][col] -= abs_val
 
-    for row in range(len(bowls)):
-        for col in range(len(bowls[row])):
-            bowls[row][col] += visited[row][col]
+    # 인접한 물고기에게 밥주는 행위
+    bowls = FeedFish(bowls)
 
     bowls = [deque(bowl) for bowl in bowls]
     tmp = []
