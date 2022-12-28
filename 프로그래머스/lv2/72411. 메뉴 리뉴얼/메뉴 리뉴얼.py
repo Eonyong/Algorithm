c1d6305350fe15cd817865e1e8c4a716915bbbfc
Eonyong <42180383+Eonyong@users.noreply.github.com>
2022-12-28 20:08:13 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 71.57 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+
+def Combinations(i, k, m, n, visited, arr, tot):
+    global menus
+    if i == m:
+        tot.sort()
+        tot = ''.join(tot)
+        menus[tot] += 1
+    else:
+        for j in range(k, n):
+            if not visited[j]:
+                visited[j] = True
+                Combinations(i + 1, j, m, n, visited, arr, tot + [arr[j]])
+                visited[j] = False
+
+def solution(orders, course):
+    global menus
+    answer = []
+    menus = defaultdict(int)
+    for order in orders:
+        n = len(order)
+        if n > 2:
+            visited = [False for _ in range(n)]
+            for idx in range(2, n + 1):
+                Combinations(0, 0, idx, n, visited, order, [])
+        else:
+            menus[order] += 1
+    tmp = defaultdict(list)
+    for k, v in menus.items():
+        if v > 1:
+            tmp[len(k)].append((k, v))
+            
+    for c in course:
+        if tmp[c]:
+            tmp[c].sort(key = lambda x: x[1])
+            val = tmp[c][-1][1]
+            while tmp[c]:
+                k, v = tmp[c].pop()
+                if v == val:
+                    answer.append(k)
+                else:
+                    break
+    answer.sort()
+    return answer
