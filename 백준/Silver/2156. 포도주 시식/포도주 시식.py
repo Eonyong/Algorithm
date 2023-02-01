368b3fae701dedc7e9e6d7669781860425c00e25
Pycho <42180383+Eonyong@users.noreply.github.com>
2023-02-01 15:17:05 +0900

    [Silver I] Title: 포도주 시식, Time: 132 ms, Memory: 115420 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,13 +1,18 @@
-n=int(input())
-array=[0]*10000
-for i in range(n):
-  array[i]=int(input())
+import sys
 
-d=[0]*10000
-d[0]=array[0]
-d[1]=array[0]+array[1]
-d[2]=max(array[2]+array[0], array[2]+array[1], d[1])
-for i in range(3,n):
-  d[i]=max(array[i]+d[i-2], array[i]+array[i-1]+d[i-3], d[i-1])
+input = sys.stdin.readline
 
-print(max(d))
+n = int(input())
+
+lis = [0 for _ in range(10000)]
+for idx in range(n):
+    lis[idx] = int(input())
+max_v = [0 for _ in range(10000)]
+max_v[0] = lis[0]
+max_v[1] = lis[0] + lis[1]
+max_v[2] = max(lis[2] + lis[0], lis[2] + lis[1], max_v[1])
+
+for idx in range(3, n):
+    max_v[idx] = max(lis[idx] + max_v[idx - 2], lis[idx] + lis[idx - 1] + max_v[idx - 3], max_v[idx - 1])
+
+print(max(max_v))
