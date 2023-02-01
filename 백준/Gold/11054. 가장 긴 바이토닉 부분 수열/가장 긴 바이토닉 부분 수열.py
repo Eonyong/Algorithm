6e70517d5b695eccf5b019e7a98182ba3bcdf547
Pycho <42180383+Eonyong@users.noreply.github.com>
2023-02-02 00:28:06 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 136 ms, Memory: 115272 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -1,25 +1,25 @@
 import sys
+
 input = sys.stdin.readline
+
+
+def Lis(n, array):
+    bitonic = [1 for _ in range(n)]
+    for start in range(n):
+        for end in range(start, n):
+            if array[start] < array[end]:
+                bitonic[end] = max(bitonic[start] + 1, bitonic[end])
+
+    return bitonic
+
 
 n = int(input())
 lis = list(map(int, input().split()))
-bitonic = [1 for _ in range(n)]
-bitonic_reverse = [1 for _ in range(n)]
 answer = 0
 
-for start in range(n):
-    for end in range(start, n):
-        if lis[start] < lis[end]:
-            bitonic[end] = max(bitonic[start] + 1, bitonic[end])
-
-lis.reverse()
-for start in range(n):
-    for end in range(start, n):
-        if lis[start] < lis[end]:
-            bitonic_reverse[end] = max(bitonic_reverse[end], bitonic_reverse[start] + 1)
-
-bitonic_reverse.reverse()
+arr = Lis(n, lis)
+revese_arr = Lis(n, lis[::-1])[::-1]
 
 for idx in range(n):
-    answer = max(answer, bitonic[idx] + bitonic_reverse[idx] - 1)
+    answer = max(answer, arr[idx] + revese_arr[idx] - 1)
 print(answer)
