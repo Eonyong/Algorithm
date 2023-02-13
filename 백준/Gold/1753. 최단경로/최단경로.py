4532125f8a0fdb8e88503dd7399dccec4eaf642f
Pycho <42180383+Eonyong@users.noreply.github.com>
2023-02-13 23:01:45 +0900

    [Gold IV] Title: 최단경로, Time: 748 ms, Memory: 163992 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,38 @@
+import sys
+import heapq
+
+def Dijkstra(middle, nodes, distances):
+    queue = []
+    heapq.heappush(queue, (0, middle))
+
+    while queue:
+        cost, node = heapq.heappop(queue)
+
+        if cost > distances[node]:
+            continue
+
+        for end, weight in nodes[node]:
+            if distances[end] > distances[node] + weight:
+                distances[end] = distances[node] + weight
+                heapq.heappush(queue, (distances[end], end))
+
+    return distances[1:n+1]
+
+n, m = map(int, input().split())
+start = int(input())
+nodes = [[] for _ in range(n + 1)]
+distances = [float('inf') for _ in range(n + 1)]
+visited = [False for _ in range(n + 1)]
+
+for _ in range(m):
+    s, e, w = map(int, input().split())
+    nodes[s].append([e, w])
+
+distances[start] = 0
+visited[start] = True
+
+for distance in Dijkstra(start, nodes, distances):
+    if distance == float('inf'):
+        print("INF")
+    else:
+        print(distance)
