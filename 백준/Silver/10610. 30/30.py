b77ad123353dfd26ce97cb88f05a6558652f44f0
Pycho <42180383+Eonyong@users.noreply.github.com>
2023-02-16 16:36:16 +0900

    [Silver IV] Title: 30, Time: 940 ms, Memory: 127900 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.py
+++ 백준/Silver/10610. 30/30.py
@@ -2,25 +2,6 @@ import sys
 
 input = sys.stdin.readline
 
-sys.setrecursionlimit(10 ** 5)
-
-
-def BigNumber(i, n, val, visited, numbers):
-    global answer
-    if i == n:
-        if not val % 30:
-            answer = max(answer, val)
-            return
-    else:
-        for j in range(n):
-            if not visited[j]:
-                visited[j] = True
-                val = val * 10 + numbers[j]
-                BigNumber(i + 1, n, val, visited, numbers)
-                visited[j] = False
-                val //= 10
-
-
 answer = 0
 numbers = list(map(int, list(input().rstrip())))
 numbers.sort(reverse=True)
@@ -31,10 +12,9 @@ for number in numbers:
     if not number:
         z_flag = True
     sumval += number
+    answer = answer * 10 + number
 
 if not sumval % 3 and z_flag:
-    for idx in range(n):
-        answer = answer * 10 + numbers[idx]
+    print(answer)
 else:
-    answer = -1
-print(answer)
+    print(-1)
