cd9b7a186ee78e4a04b2285d8dcb4a5d39d8875f
Pacho <42180383+Eonyong@users.noreply.github.com>
2023-05-28 18:49:14 +0900

    [Silver III] Title: 칸토어 집합, Time: 256 ms, Memory: 162432 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def Kantoer(n, lines):
+    a, b, c = lines[:n // 3], lines[n // 3: (2 * n) // 3], lines[(2 * n) // 3:]
+    if n == 3:
+        return '{0} {1}'.format(''.join(a), ''.join(c))
+    else:
+        return ''.join(Kantoer(n // 3, a)) + ' ' * (n // 3) + ''.join(Kantoer(n // 3, c))
+
+
+while True:
+    try:
+        n = int(input())
+        lines = ['-' for _ in range(3 ** n)]
+        if not n:
+            print('-')
+        else:
+            print(Kantoer(3 ** n, lines))
+    except:
+        break
